37cc07a96eefddedaf5fabbbebe16b1f07bbf84b
David Surya <suryadavid@ymail.com>
2024-05-18 00:37:12 +0700

    (+) revamp code (+) add some comment to explain (+) change to non static instead

--- Tsp.App/Services/TspService.cs
+++ Tsp.App/Services/TspService.cs
@@ -1,83 +1,141 @@
 namespace Tsp.App.Services;
 
-public class TspService
-{
-    public static List<List<string>> Run(string start, List<string> buildings, Dictionary<(string, string), int> distances)
-    {
-        List<List<string>> shortestPaths = [];
+public class TspService(
+    string start,
+    List<string> _buildings,
+    Dictionary<(string, string), int> _distances
+) {
+    private readonly string _start = start;
+    private readonly List<string> _buildings = _buildings;
+    private readonly Dictionary<(string, string), int> _distances = _distances;
 
-        foreach (string building in buildings)
+    public List<(List<string>, int)> GetPaths()
+    {
+        List<(List<string>, int)> shortestPaths = [];
+
+        foreach (string building in _buildings)
         {
-            if (building != start)
+            if (building != _start)
             {
-                List<string> path = DijkstraAlgo(start, building, buildings, distances);
+                (List<string> path, int length) = FindShortestPathDijkstra(building);
 
                 // check if any duplicate path (Example shortestPaths has A->D->B and path is A->D)
-                if (shortestPaths.Count > 0 && shortestPaths.Any(x => x.Take(2).SequenceEqual(path.Take(2))))
+                if (shortestPaths.Count > 0 && shortestPaths.Any(x => x.Item1.Take(2).SequenceEqual(path.Take(2))))
                 {
                     // get duplicate
-                    List<string> duplicate = shortestPaths.Where(x => x.Take(2).SequenceEqual(path.Take(2))).First();
+                    (List<string>, int) duplicate = shortestPaths.Where(x => x.Item1.Take(2).SequenceEqual(path.Take(2))).First();
 
                     // condition to check if path is longer than existing, if true then remove it, else just continue the code
-                    if (duplicate.Count < path.Count)
+                    if (duplicate.Item1.Count < path.Count)
                     {
                         shortestPaths.Remove(duplicate);
                     }
+
                     else
                     {
                         continue;
                     }
                 }
 
-                shortestPaths.Add(path);
+                shortestPaths.Add((path, length));
             }
         }
 
         return shortestPaths;
     }
 
-    public static int GetLengths(List<string> path, Dictionary<(string, string), int> distances)
+    private int GetLengths(List<string> path)
     {
         int length = 0;
+
         for (int i = 0; i < path.Count - 1; i++)
         {
             (string, string) key = (path[i], path[i + 1]);
-            if (distances.TryGetValue(key, out int value))
+            if (_distances.TryGetValue(key, out int value))
             {
                 length += value;
             }
         }
+
         return length;
     }
 
-    private static List<string> DijkstraAlgo(string start, string end, List<string> buildings, Dictionary<(string, string), int> distances)
+    private (List<string>, int) FindShortestPathDijkstra(string end)
+    {
+        (Dictionary<string, int> distance, Dictionary<string, string> previous, HashSet<string> unvisited) = InitializeVariable();
+
+        CheckPathLengthPerNeighbor(end, distance, previous, unvisited);
+
+        return ProcessPath(end, previous);
+    }
+
+    private static string GetClosestBuilding(Dictionary<string, int> dist, HashSet<string> unvisited)
+    {
+        // use max value to make it easier to compare rather than using 0 since we need to check for shorter path rather than longer path
+        int minDistance = int.MaxValue;
+        string closestBuilding = null;
+
+        foreach (string building in unvisited)
+        {
+            int currentDistance = dist.First(x => x.Key == building).Value;
+            if (currentDistance < minDistance)
+            {
+                minDistance = currentDistance;
+                closestBuilding = building;
+            }
+        }
+
+        return closestBuilding;
+    }
+
+    private (Dictionary<string, int>, Dictionary<string, string>, HashSet<string>) InitializeVariable()
     {
         Dictionary<string, int> distance = [];
         Dictionary<string, string> previous = [];
-        HashSet<string> unvisited = new(buildings);
 
-        foreach (string building in buildings)
+        // make variable to list all unvisited building
+        HashSet<string> unvisited = new(_buildings);
+
+        foreach (string building in _buildings)
         {
-            distance[building] = int.MaxValue;
-            previous[building] = null;
+            distance.Add(building, int.MaxValue);
+            previous.Add(building, null);
         }
-        distance[start] = 0;
 
+        distance[_start] = 0;
+
+        return (distance, previous, unvisited);
+    }
+
+    private void CheckPathLengthPerNeighbor(string end, Dictionary<string, int> distance, Dictionary<string, string> previous, HashSet<string> unvisited)
+    {
         while (unvisited.Count > 0)
         {
-            string current = GetClosestCity(distance, unvisited);
+            string current = GetClosestBuilding(distance, unvisited);
+
+            // remove it since it has been "visited" currently
             unvisited.Remove(current);
 
+            // since we dont need to loop on the same city, just break the loop and goes to other logic
             if (current == end)
-                break;
-
-            foreach (string neighbor in buildings)
             {
+                break;
+            }
+
+            foreach (string neighbor in _buildings)
+            {
+                // get key for variable distances to check if there is any relation from current building to current neighbor
                 (string, string) key = (current, neighbor);
-                if (distances.TryGetValue(key, out int value) && unvisited.Contains(neighbor))
+
+                // try to get distance from previous key and check if neighbor is unvisited or not
+                if (_distances.TryGetValue(key, out int value) && unvisited.Contains(neighbor))
                 {
-                    int alt = distance[current] + value;
-                    if (alt < distance[neighbor])
+                    // if found, add the distance with value from the condition before as new variable
+                    int alt = distance.First(x => x.Key == current).Value + value;
+                    int distanceNeighbor = distance.First(x => x.Key == neighbor).Value;
+
+                    // if new variable is shorter than current distance from building to neighbor than replace it
+                    if (alt < distanceNeighbor)
                     {
                         distance[neighbor] = alt;
                         previous[neighbor] = current;
@@ -85,31 +143,20 @@ public class TspService
                 }
             }
         }
+    }
 
+    private (List<string>, int) ProcessPath(string end, Dictionary<string, string> previous)
+    {
         List<string> path = [];
+
         for (string at = end; at != null; at = previous[at])
         {
             path.Add(at);
         }
+
+        // reverse it since it was started from "end"
         path.Reverse();
 
-        return path;
-    }
-
-    private static string GetClosestCity(Dictionary<string, int> dist, HashSet<string> unvisited)
-    {
-        int minDistance = int.MaxValue;
-        string closestCity = null;
-
-        foreach (string building in unvisited)
-        {
-            if (dist[building] < minDistance)
-            {
-                minDistance = dist[building];
-                closestCity = building;
-            }
-        }
-
-        return closestCity;
+        return (path, GetLengths(path));
     }
 }
